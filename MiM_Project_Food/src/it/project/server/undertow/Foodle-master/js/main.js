1af498114079c8986714232c67cf18c8a2e74fcf
lucabrombin <lbrombin@gmail.com>
2019-06-09 13:58:14 +0200

    sending image for similarity
    aggiunto l'invio dell'immagine al server;
    manca callback

--- MiM_Project_Food/src/it/project/server/undertow/Foodle-master/js/main.js
+++ MiM_Project_Food/src/it/project/server/undertow/Foodle-master/js/main.js
@@ -1,3 +1,53 @@
+var socket;
+if (window.WebSocket) {
+    socket = new WebSocket("ws://localhost:8100/endpoint");
+    socket.onmessage = function (event) {
+        var chat = document.getElementById('chat');
+        var obj = JSON.parse(event.data);
+        var i = 0;
+        var dim = Object.keys(obj.imgs).length;
+        for(i = 0; i < dim; i++){
+          var div = document.createElement('div');
+          var classe = obj.imgs[i].classe;
+          var nameImg = "url('./img/"+classe+"/" + obj.imgs[i].img + "')";
+          div.style.backgroundImage = nameImg;
+          div.style.width = "500";
+          div.style.height = "500";
+          div.style.display = "inline-block";
+          div.style.float="left";
+
+          chat.appendChild(div);
+        }
+        //chat.innerHTML = event.data + "<br />";
+    };
+} else {
+    alert("Your browser does not support Websockets. (Use Chrome)");
+}
+
+function encodeImageFileAsURL() {
+
+    var filesSelected = document.getElementById("filechooser").files;
+    if (filesSelected.length > 0) {
+        var fileToLoad = filesSelected[0];
+
+        var fileReader = new FileReader();
+
+        fileReader.onload = function(fileLoadedEvent) {
+            var srcData = fileLoadedEvent.target.result; // <--- data: base64
+            //var chat = document.getElementById('chat');
+            //chat.innerHTML = srcData;
+            socket.send(srcData)
+            var newImage = document.createElement('img');
+            newImage.src = srcData;
+
+            document.getElementById("imgTest").innerHTML = newImage.outerHTML;
+            //alert("Converted Base64 version is " + document.getElementById("imgTest").innerHTML);
+            //console.log("Converted Base64 version is " + document.getElementById("imgTest").innerHTML);
+        }
+        fileReader.readAsDataURL(fileToLoad);
+    }
+}
+
 var test_msg = '{"imgs":[{"img":"803481.jpg","classe":"beef_carpaccio"},{"img":"1242241.jpg","classe":"breakfast_burrito"},{"img":"3607592.jpg","classe":"breakfast_burrito"},{"img":"820545.jpg","classe":"bruschetta"},{"img":"206027.jpg","classe":"beef_carpaccio"},{"img":"212368.jpg","classe":"beef_carpaccio"},{"img":"1172580.jpg","classe":"bruschetta"},{"img":"3429450.jpg","classe":"bibimbap"},{"img":"3051510.jpg","classe":"bibimbap"},{"img":"1832620.jpg","classe":"beef_carpaccio"},{"img":"3503609.jpg","classe":"beef_carpaccio"},{"img":"2472060.jpg","classe":"baby_back_ribs"},{"img":"3348442.jpg","classe":"beef_carpaccio"},{"img":"3837961.jpg","classe":"baby_back_ribs"},{"img":"1191063.jpg","classe":"bibimbap"},{"img":"3766342.jpg","classe":"baklava"},{"img":"1585661.jpg","classe":"bread_pudding"},{"img":"3441400.jpg","classe":"breakfast_burrito"},{"img":"3660558.jpg","classe":"beef_carpaccio"},{"img":"318343.jpg","classe":"beet_salad"},{"img":"1162327.jpg","classe":"beef_carpaccio"},{"img":"2221152.jpg","classe":"beet_salad"},{"img":"2024914.jpg","classe":"baklava"},{"img":"3187646.jpg","classe":"bibimbap"},{"img":"374126.jpg","classe":"beet_salad"},{"img":"1600317.jpg","classe":"baklava"},{"img":"239826.jpg","classe":"beet_salad"},{"img":"1894239.jpg","classe":"beef_carpaccio"},{"img":"466260.jpg","classe":"baby_back_ribs"},{"img":"913291.jpg","classe":"beef_tartare"}]}';
 var best_result = {};
 var related_food = {};
@@ -41,8 +91,27 @@ function parse_json_collection(data) {
 
 function submit_picture(){
   if (this.files && this.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
+    //var reader = new FileReader();
+
+    var fileToLoad = this.files[0];
+    var fileReader = new FileReader();
+
+    fileReader.onload = function(fileLoadedEvent) {
+        var srcData = fileLoadedEvent.target.result; // <--- data: base64
+        //var chat = document.getElementById('chat');
+        //chat.innerHTML = srcData;
+        socket.send(srcData)
+        //var newImage = document.createElement('img');
+        //newImage.src = srcData;
+
+        //document.getElementById("imgTest").innerHTML = newImage.outerHTML;
+        //alert("Converted Base64 version is " + document.getElementById("imgTest").innerHTML);
+        //console.log("Converted Base64 version is " + document.getElementById("imgTest").innerHTML);
+    }
+    fileReader.readAsDataURL(fileToLoad);
+
+    /*
+    reader.onload = function(e) {      
 
       ///////
       // actually all this stuff should be done in the callback after the upload 
@@ -58,6 +127,7 @@ function submit_picture(){
       ///////
     }
     reader.readAsDataURL(this.files[0]);
+  }*/
   }
 }
 
